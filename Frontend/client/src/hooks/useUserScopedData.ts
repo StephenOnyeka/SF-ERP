707eca5c44794a6bbdf56210784c48c223c37b13
richmondnkrumah <richmondnkrumah04@gmail.com>
2025-06-24 06:23:04 +0000

    refactored to use new auth hook

--- Frontend/client/src/hooks/useUserScopedData.ts
+++ Frontend/client/src/hooks/useUserScopedData.ts
@@ -2,18 +2,19 @@ import { useAuthStore } from '@/stores/auth-store';
 import { useAttendanceStore } from '@/stores/attendance-store';
 import { useLeaveStore } from '@/stores/leave-store';
 import { useSalaryStore } from '@/stores/salary-store';
+import { useAuth } from './use-auth';
 
 export const useUserScopedData = () => {
-  const user = useAuthStore((s) => s.user)!;
+  const {user} = useAuth()
   const attendance = useAttendanceStore((s) => s.attendanceRecords);
   const leaves = useLeaveStore((s) => s.leaveApplications);
   const quotas = useLeaveStore((s) => s.leaveQuotas);
   const salary =  useSalaryStore((s) => s.salaryRecords)
 
-  const userAttendance = attendance.filter((rec) => rec.userId === user.id);
-  const userLeaves = leaves.filter((rec) => rec.userId === user.id);
-  const userQuotas = quotas.filter((rec) => rec.userId === user.id);
-  const salaryRecords = salary.filter((rec) => rec.userId === user.id)
+  const userAttendance = attendance.filter((rec) => rec.userId === user?.id);
+  const userLeaves = leaves.filter((rec) => rec.userId === user?.id);
+  const userQuotas = quotas.filter((rec) => rec.userId === user?.id);
+  const salaryRecords = salary.filter((rec) => rec.userId === user?.id)
 
   return {
     user,
